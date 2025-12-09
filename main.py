5f8d4e7d78f4c441da2f3f9a5e89b0964ef9a9f1
vladjavadev <vharcuk02@gmail.com>
2025-12-09 21:19:52 +0200

    marker detect

---
+++ main.py
@@ -0,0 +1,38 @@
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+import time
+
+
+start = time.time()
+# Завантажуємо фото з маркером
+frame = cv2.imread("img/image2.png")
+
+# Словник маркерів
+dictionary = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
+
+# Детекція маркерів
+corners, ids, rejected = aruco.detectMarkers(frame, dictionary)
+
+# Параметри камери (треба калібрувати для точності!)
+# Тут приклад з умовними значеннями
+camera_matrix = np.array([[1000, 0, frame.shape[1]/2],
+                          [0, 1000, frame.shape[0]/2],
+                          [0, 0, 1]], dtype=np.float32)
+dist_coeffs = np.zeros((5,1))  # якщо немає калібрування
+
+# Оцінка позиції
+if ids is not None:
+    rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, 0.05, camera_matrix, dist_coeffs)
+    for i in range(len(ids)):
+        print(f"Маркер {ids[i]}:")
+        print("  rvec:", rvecs[i])  # вектор обертання
+        print("  tvec:", tvecs[i])  # вектор трансляції
+
+        # Малюємо координатні осі
+        cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvecs[i], tvecs[i], 0.05)
+
+cv2.imshow("Pose Estimation", frame)
+print("Time: ",time.time()-start)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
