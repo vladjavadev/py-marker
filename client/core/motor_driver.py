626aa7c3757afca73f6e183a368dd98234e93ff3
vladjavadev <vharcuk02@gmail.com>
2025-12-20 15:36:29 +0200

    rewrite motor driver

--- client/core/motor_driver.py
+++ client/core/motor_driver.py
@@ -10,17 +10,21 @@ class DIR(Enum):
 
 
 class MotorDriver:
-    """Real MotorDriver for BeagleBone Black hardware"""
+    """Real MotorDriver for BeagleBone Black hardware with TB6612FNG driver"""
     
-    # Beide Motoren werden mit unterschiedlichen PWM angesteuert
-    pin_FmotL = "P9_14"  # GPIO 50 PWMA linker Motor (Forward)
-    pin_FmotR = "P9_21"  # GPIO 51 PWMB rechter Motor (Forward)
+    # PWM pins
+    pin_PWMA = "P8_21"  # PWM for Motor A (left motor)
+    pin_PWMB = "P8_22"  # PWM for Motor B (right motor)
+    
+    # Motor A (left) direction control pins
+    pin_AIN1 = "P8_27"  # Motor A input 1
+    pin_AIN2 = "P8_29"  # Motor A input 2
+    
+    # Motor B (right) direction control pins
+    pin_BIN1 = "P8_25"  # Motor B input 1
+    pin_BIN2 = "P8_23"  # Motor B input 2
 
-    pin_BmotL = "P9_16"  # GPIO 50 PWMA linker Motor (Backward)
-    pin_BmotR = "P9_22"  # GPIO 51 PWMB rechter Motor (Backward)
-    # Eingänge In2 und In2 invertiert zueinander (geben Drehrichtung vor)
-
-    FREQ = 250000
+    FREQ = 25000  # 25kHz PWM frequency (typical for TB6612FNG)
     pwm_max = 90
     pwm_min = 30
 
@@ -30,11 +34,21 @@ class MotorDriver:
         Args:
             v_max: Maximum velocity in mm/s
         """
-        # Start all PWM channels at 0% duty cycle
-        PWM.start(self.pin_FmotL, 0, self.FREQ, 0)
-        PWM.start(self.pin_FmotR, 0, self.FREQ, 0)
-        PWM.start(self.pin_BmotL, 0, self.FREQ, 0)
-        PWM.start(self.pin_BmotR, 0, self.FREQ, 0)
+        # Setup direction pins as outputs
+        GPIO.setup(self.pin_AIN1, GPIO.OUT)
+        GPIO.setup(self.pin_AIN2, GPIO.OUT)
+        GPIO.setup(self.pin_BIN1, GPIO.OUT)
+        GPIO.setup(self.pin_BIN2, GPIO.OUT)
+        
+        # Initialize direction pins to LOW (stopped)
+        GPIO.output(self.pin_AIN1, GPIO.LOW)
+        GPIO.output(self.pin_AIN2, GPIO.LOW)
+        GPIO.output(self.pin_BIN1, GPIO.LOW)
+        GPIO.output(self.pin_BIN2, GPIO.LOW)
+        
+        # Start PWM channels at 0% duty cycle
+        PWM.start(self.pin_PWMA, 0, self.FREQ, 0)
+        PWM.start(self.pin_PWMB, 0, self.FREQ, 0)
         
         self.v_max = v_max
         self.pwm_l = 0
@@ -62,27 +76,39 @@ class MotorDriver:
         else:
             self.pwm_r = 0
 
-        # Left motor control
+        # Left motor (Motor A) control
+        # Direction: AIN1=HIGH, AIN2=LOW -> Forward
+        #           AIN1=LOW, AIN2=HIGH -> Reverse
+        #           AIN1=LOW, AIN2=LOW  -> Stop (brake)
         if duty_l > 0:  # Forward
-            PWM.set_duty_cycle(self.pin_BmotL, 0)
-            PWM.set_duty_cycle(self.pin_FmotL, self.pwm_l)
+            GPIO.output(self.pin_AIN1, GPIO.HIGH)
+            GPIO.output(self.pin_AIN2, GPIO.LOW)
+            PWM.set_duty_cycle(self.pin_PWMA, self.pwm_l)
         elif duty_l < 0:  # Reverse
-            PWM.set_duty_cycle(self.pin_FmotL, 0)
-            PWM.set_duty_cycle(self.pin_BmotL, self.pwm_l)
+            GPIO.output(self.pin_AIN1, GPIO.LOW)
+            GPIO.output(self.pin_AIN2, GPIO.HIGH)
+            PWM.set_duty_cycle(self.pin_PWMA, self.pwm_l)
         else:  # Stop
-            PWM.set_duty_cycle(self.pin_FmotL, 0)
-            PWM.set_duty_cycle(self.pin_BmotL, 0)
+            GPIO.output(self.pin_AIN1, GPIO.LOW)
+            GPIO.output(self.pin_AIN2, GPIO.LOW)
+            PWM.set_duty_cycle(self.pin_PWMA, 0)
 
-        # Right motor control
+        # Right motor (Motor B) control
+        # Direction: BIN1=HIGH, BIN2=LOW -> Forward
+        #           BIN1=LOW, BIN2=HIGH -> Reverse
+        #           BIN1=LOW, BIN2=LOW  -> Stop (brake)
         if duty_r > 0:  # Forward
-            PWM.set_duty_cycle(self.pin_BmotR, 0)
-            PWM.set_duty_cycle(self.pin_FmotR, self.pwm_r)
+            GPIO.output(self.pin_BIN1, GPIO.HIGH)
+            GPIO.output(self.pin_BIN2, GPIO.LOW)
+            PWM.set_duty_cycle(self.pin_PWMB, self.pwm_r)
         elif duty_r < 0:  # Reverse
-            PWM.set_duty_cycle(self.pin_FmotR, 0)
-            PWM.set_duty_cycle(self.pin_BmotR, self.pwm_r)
+            GPIO.output(self.pin_BIN1, GPIO.LOW)
+            GPIO.output(self.pin_BIN2, GPIO.HIGH)
+            PWM.set_duty_cycle(self.pin_PWMB, self.pwm_r)
         else:  # Stop
-            PWM.set_duty_cycle(self.pin_FmotR, 0)
-            PWM.set_duty_cycle(self.pin_BmotR, 0)
+            GPIO.output(self.pin_BIN1, GPIO.LOW)
+            GPIO.output(self.pin_BIN2, GPIO.LOW)
+            PWM.set_duty_cycle(self.pin_PWMB, 0)
 
     def set_motor_speed(self, vl, vr):
         """Set motor speed in mm/s (legacy method for compatibility)
@@ -103,30 +129,40 @@ class MotorDriver:
         self.set_wheel_duty(duty_l, duty_r)
 
     def stop(self):
-        """Emergency stop - set all motors to 0"""
-        PWM.set_duty_cycle(self.pin_FmotL, 0)
-        PWM.set_duty_cycle(self.pin_FmotR, 0)
-        PWM.set_duty_cycle(self.pin_BmotL, 0)
-        PWM.set_duty_cycle(self.pin_BmotR, 0)
+        """Emergency stop - set all motors to brake mode"""
+        # Set all direction pins LOW for brake
+        GPIO.output(self.pin_AIN1, GPIO.LOW)
+        GPIO.output(self.pin_AIN2, GPIO.LOW)
+        GPIO.output(self.pin_BIN1, GPIO.LOW)
+        GPIO.output(self.pin_BIN2, GPIO.LOW)
+        
+        # Set PWM to 0
+        PWM.set_duty_cycle(self.pin_PWMA, 0)
+        PWM.set_duty_cycle(self.pin_PWMB, 0)
+        
         self.pwm_l = 0
         self.pwm_r = 0
 
     def cleanup(self):
-        """Clean up PWM resources"""
+        """Clean up PWM and GPIO resources"""
         self.stop()
-        PWM.stop(self.pin_FmotL)
-        PWM.stop(self.pin_FmotR)
-        PWM.stop(self.pin_BmotL)
-        PWM.stop(self.pin_BmotR)
+        PWM.stop(self.pin_PWMA)
+        PWM.stop(self.pin_PWMB)
         PWM.cleanup()
+        GPIO.cleanup()
 
 
 # Test code for hardware verification
 if __name__ == "__main__":
     import time
     
-    print("=== Testing MotorDriver on BeagleBone ===")
-    print("WARNING: Motors will run! Make sure robot is on blocks or safely secured.\n")
+    print("=== Testing MotorDriver on BeagleBone with TB6612FNG ===")
+    print("Pin Configuration:")
+    print("  PWMA (P8_21) - Left Motor Speed")
+    print("  PWMB (P8_22) - Right Motor Speed")
+    print("  AIN1 (P8_27), AIN2 (P8_29) - Left Motor Direction")
+    print("  BIN1 (P8_25), BIN2 (P8_23) - Right Motor Direction")
+    print("\nWARNING: Motors will run! Make sure robot is on blocks or safely secured.\n")
     
     try:
         driver = MotorDriver(v_max=200)
@@ -177,5 +213,5 @@ if __name__ == "__main__":
         print(f"\n\nError during test: {e}")
         driver.stop()
     finally:
-        print("Cleaning up PWM resources")
+        print("Cleaning up resources")
         driver.cleanup()
